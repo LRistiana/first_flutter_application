697b171d6a63ea1fe74ddf95d64c836da3b1acf6
Lristiana <liangga4@gmail.com>
2024-06-01 19:50:34 +0800

    feat : adding theme package for color and size

---
+++ lib/utils/theme/color_theme.dart
@@ -0,0 +1,16 @@
+import "package:flutter/material.dart";
+
+class GeneralColor{
+  static const Color primaryColor = Color.fromRGBO(215, 252, 112, 1);
+  static const Color darkColor = Colors.black;
+  static const Color lightColor = Colors.white;
+}
+class BackgroundColor{
+  static const Color primaryBackgroundColor = Color.fromRGBO(17, 17, 17, 1);
+  static const Color secondaryBackgroundColor = Colors.black;
+}
+
+class TextColor{
+  static const Color lightTextColor = Colors.white;
+  static const Color darkTextColor = Colors.black;
+}
---
+++ lib/utils/theme/size_theme.dart
@@ -0,0 +1,11 @@
+class TextSize {
+  static const double small = 12.0;
+  static const double medium = 16.0;
+  static const double large = 20.0;
+  static const double xlarge = 24.0;
+}
+
+class BorderRadiusSize {
+  static const double box = 8.0;
+  static const double card = 12.0;
+}
